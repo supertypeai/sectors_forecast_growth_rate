f616f0f72c19725074621cd51f63fbd7ee261713
unknown <projectwilsen@gmail.com>
2023-11-15 10:14:00 +0700

    add

--- coba.py
+++ coba.py
@@ -9,53 +9,6 @@ import requests
 from datetime import datetime
 from io import StringIO
 
-url = os.environ.get("SUPABASE_URL")
-key = os.environ.get("SUPABASE_KEY")
-supabase = create_client(url, key)
-
-key_data = supabase.table("idx_company_profile").select("symbol","sub_sector_id").execute()
-key_df = pd.DataFrame(key_data.data).sort_values(['symbol'])
-
-symbols = key_df['symbol'].to_list()
-
-headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/113.0'}
-
-all_list = {
-    'revenue_year_ago' : []
-}
-
-for symbol in symbols:
-    try:
-        url = f'https://finance.yahoo.com/quote/{symbol}/analysis?p={symbol}'
-        html_content = requests.get(url, headers=headers).text
-        source_df = pd.read_html(StringIO(html_content))
-
-        revenue_df = source_df[1]
-
-        year_ago = revenue_df.loc[revenue_df['Revenue Estimate'] == 'Year Ago Sales'].iloc[:, 3].values[0]
-
-        all_list['revenue_year_ago'].append(year_ago)
-
-        print(f"{symbol} data processed")
-    except Exception as e:
-        for key in all_list.keys():
-            all_list[key].append(np.nan)
-        print(f"{symbol} no data")
-
-data_dict = {
-    'symbol': symbols,  
-    **all_list,  
-}
-
-df = pd.DataFrame.from_dict(data_dict)
-
-current_year = datetime.now().year
-last_year= f"{current_year-1}-12-31"
-f_data = supabase.table("idx_financials_annual").select("symbol","total_revenue","basic_eps").eq("date", last_year).execute()
-f_df = pd.DataFrame(f_data.data).sort_values(['symbol'])
-
-rev_year_ago_df = df.merge(f_df, on='symbol', how='inner').merge(key_df, on='symbol', how='inner')
-
 def preprocess_numeric_value(value):
     if pd.isna(value) or value == 0.0:
         return np.nan
@@ -70,9 +23,100 @@ def preprocess_numeric_value(value):
     else:
         return float(value)
 
-numeric_columns = ['revenue_year_ago']
+def preprocess_percentage_value(value):
+    if pd.isna(value):
+        return np.nan
+    if '%' in str(value):
+        return float(str(value).replace('%', '').replace(',', ''))/100
+    else:
+        return float(str(value))
+
+url = os.environ.get("SUPABASE_URL")
+key = os.environ.get("SUPABASE_KEY")
+supabase = create_client(url, key)
+
+key_data = supabase.table("idx_company_profile").select("symbol","sub_sector_id").execute()
+key_df = pd.DataFrame(key_data.data).sort_values(['symbol'])
+
+symbols = key_df['symbol'].to_list()
+
+headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/113.0'}
+
+all_list = {
+    'revenue_year_ago' : [],
+    'avg_estimate_earnings_current_year': [],
+    'avg_estimate_earnings_next_year': [],
+    'avg_estimate_revenue_current_year': [],
+    'avg_estimate_revenue_next_year': [],
+    'estimate_overall_growth_current_year': [],
+    'estimate_overall_growth_next_year': [],
+    'estimate_overall_growth_next_five_years': [],
+}
+
+for symbol in symbols:
+    try:
+        url = f'https://finance.yahoo.com/quote/{symbol}/analysis?p={symbol}'
+        html_content = requests.get(url, headers=headers).text
+        source_df = pd.read_html(StringIO(html_content))
+
+        earnings_df = source_df[0]
+        revenue_df = source_df[1]
+        overall_growth_df = source_df[5]
+
+        avg_estimate_earnings_current_year = earnings_df.loc[earnings_df['Earnings Estimate'] == 'Avg. Estimate'].iloc[:, 3].values[0]
+        avg_estimate_earnings_next_year = earnings_df.loc[earnings_df['Earnings Estimate'] == 'Avg. Estimate'].iloc[:, 4].values[0]
+        avg_estimate_revenue_current_year = revenue_df.loc[revenue_df['Revenue Estimate'] == 'Avg. Estimate'].iloc[:, 3].values[0]
+        year_ago = revenue_df.loc[revenue_df['Revenue Estimate'] == 'Year Ago Sales'].iloc[:, 3].values[0]
+        avg_estimate_revenue_next_year = revenue_df.loc[revenue_df['Revenue Estimate'] == 'Avg. Estimate'].iloc[:, 4].values[0]
+        estimate_overall_growth_current_year = overall_growth_df.loc[overall_growth_df['Growth Estimates'] == 'Current Year'].iloc[0, :].values[1]
+        estimate_overall_growth_next_year = overall_growth_df.loc[overall_growth_df['Growth Estimates'] == 'Next Year'].iloc[0, :].values[1]
+        estimate_overall_growth_next_five_years = overall_growth_df.loc[overall_growth_df['Growth Estimates'] == 'Next 5 Years (per annum)'].iloc[0, :].values[1]
+
+        all_list['avg_estimate_earnings_current_year'].append(avg_estimate_earnings_current_year)
+        all_list['avg_estimate_earnings_next_year'].append(avg_estimate_earnings_next_year)
+        all_list['avg_estimate_revenue_current_year'].append(avg_estimate_revenue_current_year)
+        all_list['avg_estimate_revenue_next_year'].append(avg_estimate_revenue_next_year)
+        all_list['revenue_year_ago'].append(year_ago)
+        all_list['estimate_overall_growth_current_year'].append(estimate_overall_growth_current_year)
+        all_list['estimate_overall_growth_next_year'].append(estimate_overall_growth_next_year)
+        all_list['estimate_overall_growth_next_five_years'].append(estimate_overall_growth_next_five_years)
+
+        print(f"{symbol} data processed")
+    except Exception as e:
+        for key in all_list.keys():
+            all_list[key].append(np.nan)
+        print(f"{symbol} no data")
+
+data_dict = {
+    'symbol': symbols,  
+    **all_list,  
+}
+
+forecast_df = pd.DataFrame.from_dict(data_dict)
+
+current_year = datetime.now().year
+last_year= f"{current_year-1}-12-31"
+db_data = supabase.table("idx_financials_annual").select("symbol","total_revenue","basic_eps").eq("date", last_year).execute()
+db_df = pd.DataFrame(db_data.data).sort_values(['symbol'])
+
+df = forecast_df.merge(db_df, on='symbol', how='inner').merge(key_df, on='symbol', how='inner')
+
+numeric_columns = ['avg_estimate_earnings_current_year', 'avg_estimate_earnings_next_year', 'avg_estimate_revenue_current_year', 'avg_estimate_revenue_next_year','revenue_year_ago']
 
 for column in numeric_columns:
-    rev_year_ago_df[column] = rev_year_ago_df[column].apply(preprocess_numeric_value)
+    df[column] = df[column].apply(preprocess_numeric_value)
 
-rev_year_ago_df.to_csv('idx_company_rev_year_ago.csv',index = False)
+percentage_columns = ['estimate_overall_growth_current_year', 'estimate_overall_growth_next_year',	'estimate_overall_growth_next_five_years']
+for percentage_column in percentage_columns:
+  growth_forecast_df[str(percentage_column)] = growth_forecast_df[str(percentage_column)].apply(preprocess_percentage_value)
+
+df['multiplier'] = 1
+df_1000 = df.copy()
+df_1000['multiplier'] = 1000
+df_1000['revenue_year_ago'] = df_1000['revenue_year_ago'] * df_1000['multiplier']
+final_df = pd.concat([df, df_1000], axis=0, ignore_index=True)
+final_df = final_df.sort_values(by=["symbol", "multiplier"])
+final_df['ratio_mult'] = final_df['total_revenue']/ final_df['revenue_year_ago']
+final_df = final_df.query("ratio_mult > 0.5 and ratio_mult < 2")
+
+final_df.to_csv('idx_company_rev_year_ago_filtered.csv',index = False)
