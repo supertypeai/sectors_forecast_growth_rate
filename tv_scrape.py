61a2a1d3f09fec81f1b4cbd425fbe7fa556fd044
Gerald Bryan <69706675+geraldbryan@users.noreply.github.com>
2024-10-25 14:49:26 +0700

    delete symbol filter test

--- tv_scrape.py
+++ tv_scrape.py
@@ -207,8 +207,6 @@ driver.maximize_window()
 
 # Start Scraping
 df_fore = pd.DataFrame()
-
-active_stock = active_stock[active_stock.symbol.isin(['BBCA','BMRI','ICBP','TPIA','CPIN'])] #TEST ONLY
                                                       
 for i in range(0,active_stock.shape[0]):
     symbol = active_stock.symbol.iloc[i]
